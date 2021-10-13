cfa9f7dc7bfb5e3de1c60e2a5e933486b4939dd4
kevinli <likevintw@gmail.com>
2021-10-13 17:55:11 +0800

    check main_get_image_url.py, ok

--- main_get_image_url.py
+++ main_get_image_url.py
@@ -23,90 +23,103 @@ def get_image_url(
     structure.export_image_url(handler)
 
 
-REMOVE_KEYWORDS = ["aws", "txt", "NG","xlsx","git"]
+REMOVE_KEYWORDS = ["aws", "txt", "NG", "xlsx", "git"]
+
 
 def run_package_positive():
+    path = "../input/arnoo_test_package"
     get_image_url(
-        input_folder_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_package",
+        input_folder_path=path,
         path_remoeve_keywords=REMOVE_KEYWORDS,
         path_keep_keywords=["jpg", "Positive"],
-        replace_remove="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_package/",
+        replace_remove=path,
         replace_add="http://192.168.5.130/upload/Package/",
-        export_file_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/temp/arnoo_image_url/package_positive.txt")
+        export_file_path="../output/image_url/package_positive.txt")
 
 
 def run_package_negative():
+    path = "../input/arnoo_test_package"
     get_image_url(
-        input_folder_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_package",
+        input_folder_path=path,
         path_remoeve_keywords=REMOVE_KEYWORDS,
         path_keep_keywords=["jpg", "Negative"],
-        replace_remove="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_package/",
+        replace_remove=path,
         replace_add="http://192.168.5.130/upload/Package/",
-        export_file_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/temp/arnoo_image_url/package_negative.txt")
+        export_file_path="../output/image_url/package_negative.txt")
 
 
 def run_human_positive():
+    path = "../input/arnoo_test_human"
     get_image_url(
-        input_folder_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_human",
+        input_folder_path=path,
         path_remoeve_keywords=REMOVE_KEYWORDS,
         path_keep_keywords=["jpg", "positive"],
-        replace_remove="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_human/",
+        replace_remove=path,
         replace_add="http://192.168.5.130/upload/Human/Human/",
-        export_file_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/temp/arnoo_image_url/human_positive.txt")
+        export_file_path="../output/image_url/human_positive.txt")
 
 
 def run_human_negative():
+    path = "../input/arnoo_test_human"
     get_image_url(
-        input_folder_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_human",
+        input_folder_path=path,
         path_remoeve_keywords=REMOVE_KEYWORDS,
         path_keep_keywords=["jpg", "negative"],
-        replace_remove="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_human/",
+        replace_remove=path,
         replace_add="http://192.168.5.130/upload/Human/Human/",
-        export_file_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/temp/arnoo_image_url/human_negative.txt")
+        export_file_path="../output/image_url/human_negative.txt")
 
 
 def run_pet_positive():
+    path = "../input/arnoo_test_pet"
     get_image_url(
-        input_folder_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_pet",
+        input_folder_path=path,
         path_remoeve_keywords=REMOVE_KEYWORDS,
         path_keep_keywords=["jpg", "posi"],
-        replace_remove="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_pet/",
+        replace_remove=path,
         replace_add="http://192.168.5.130/upload/Pet/Pet/",
-        export_file_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/temp/arnoo_image_url/pet_positive.txt")
+        export_file_path="../output/image_url/pet_positive.txt")
 
 
 def run_pet_negative():
+    path = "../input/arnoo_test_pet"
     get_image_url(
-        input_folder_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_pet",
+        input_folder_path=path,
         path_remoeve_keywords=REMOVE_KEYWORDS,
         path_keep_keywords=["jpg", "negative"],
-        replace_remove="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_pet/",
+        replace_remove=path,
         replace_add="http://192.168.5.130/upload/Pet/Pet/",
-        export_file_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/temp/arnoo_image_url/pet_negative.txt")
+        export_file_path="../output/image_url/pet_negative.txt")
+
+
 def run_vehicle_positive():
+    path = "../input/arnoo_test_vehicle"
     get_image_url(
-        input_folder_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_vehicle",
+        input_folder_path=path,
         path_remoeve_keywords=REMOVE_KEYWORDS,
         path_keep_keywords=["jpg", "positive"],
-        replace_remove="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_vehicle/",
+        replace_remove=path,
         replace_add="http://192.168.5.130/upload/Vehicle/",
-        export_file_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/temp/arnoo_image_url/vehicle_positive.txt")
+        export_file_path="../output/image_url/vehicle_positive.txt")
+
 
 def run_vehicle_negative():
+    path = "../input/arnoo_test_vehicle"
     get_image_url(
-        input_folder_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_vehicle",
+        input_folder_path=path,
         path_remoeve_keywords=REMOVE_KEYWORDS,
         path_keep_keywords=["jpg", "negative"],
-        replace_remove="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/arnoo_test_vehicle/",
+        replace_remove=path,
         replace_add="http://192.168.5.130/upload/Vehicle/",
-        export_file_path="/mnt/c/Users/LDS/Desktop/workingspace/leedarson_workingspace/temp/arnoo_image_url/vehicle_negative.txt")
+        export_file_path="../output/image_url/vehicle_negative.txt")
+
 
 if __name__ == '__main__':
-    run_package_positive()
-    run_package_negative()
     run_human_positive()
     run_human_negative()
     run_pet_positive()
     run_pet_negative()
     run_vehicle_positive()
     run_vehicle_negative()
+    run_package_positive()
+    run_package_negative()
